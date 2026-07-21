2c48bddbdd3031d1694c9026e83bdeb821174ff7
Iván Sarió Madrigal <isario@odec.es>
2026-07-21 10:28:12 +0200

    fix: Develop get elements to get forward with crud implementations

--- src/main/java/com/worktrack/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/worktrack/exceptions/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,18 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends Exception {
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException ex) {
-        log.error("User/Users Not Found: {}", ex.getMessage());
-        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage());
-    }
-
-    @ExceptionHandler(InvalidAttributeUserException.class)
-    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(InvalidAttributeUserException ex) {
-        log.error("Attributes are not valid: {}", ex.getMessage());
-        return buildResponse(HttpStatus.NOT_ACCEPTABLE, ex.getMessage());
-    }
 
     private ResponseEntity<Map<String, Object>> buildResponse(HttpStatus status, String message) {
 		Map<String, Object> body = new LinkedHashMap<>();
